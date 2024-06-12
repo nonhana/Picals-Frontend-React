3b4ffc0b8ea9a5076c6004c652fab00e9d57ba10
non_hana <105428118+nonhana@users.noreply.github.com>
2024-06-12 19:01:32 +0800

    feat: 正在维修...... (#62)
    * feat: 正在维修......
    
    * feat: 注释掉引用的依赖

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,20 @@ import ReactDOM from 'react-dom/client'
 import 'react-photo-view/dist/react-photo-view.css'
 import '@unocss/reset/normalize.css'
 import 'uno.css'
-import { persistor, store } from './store'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import router from './router'
-import { RouterProvider } from 'react-router-dom'
+// import { persistor, store } from './store'
+// import { Provider } from 'react-redux'
+// import { PersistGate } from 'redux-persist/integration/react'
+// import router from './router'
+// import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <RouterProvider fallbackElement={<p>初始化加载...</p>} router={router} />
-    </PersistGate>
-  </Provider>,
+  // <Provider store={store}>
+  //   <PersistGate loading={null} persistor={persistor}>
+  //     <RouterProvider fallbackElement={<p>初始化加载...</p>} router={router} />
+  //   </PersistGate>
+  // </Provider>,
+  <div>
+    <h1>Under Maintenance</h1>
+    <p>Sorry, we are currently under maintenance. Please come back later.</p>
+  </div>,
 )
