38871c698454a0ed18144e566ee68f217a21a14e
non_hana <1209220829@qq.com>
2024-06-12 18:57:52 +0800

    feat: 正在维修......

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,13 @@ import router from './router'
 import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <RouterProvider fallbackElement={<p>初始化加载...</p>} router={router} />
-    </PersistGate>
-  </Provider>,
+  // <Provider store={store}>
+  //   <PersistGate loading={null} persistor={persistor}>
+  //     <RouterProvider fallbackElement={<p>初始化加载...</p>} router={router} />
+  //   </PersistGate>
+  // </Provider>,
+  <div>
+    <h1>Under Maintenance</h1>
+    <p>Sorry, we are currently under maintenance. Please come back later.</p>
+  </div>,
 )
