a68a1c7b93101eeb579b2c9341afb216afba88fb
non_hana <1209220829@qq.com>
2024-07-05 15:29:36 +0800

    feat: 优化推荐作品展示逻辑

--- src/components/explore/user-list/index.tsx
+++ src/components/explore/user-list/index.tsx
@@ -3,9 +3,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { AppState } from '@/store/types'
 import type { UserItemInfo } from '@/utils/types'
 import UserItem from '@/components/common/user-item'
-import { useMap, useAtBottom } from '@/hooks'
+import { useAtBottom } from '@/hooks'
 import { getRecommendUserListAPI, getWorkSimpleAPI, likeActionsAPI, userActionsAPI } from '@/apis'
-import Empty from '@/components/common/empty'
 import { message } from 'antd'
 import { increaseFollowNum, decreaseFollowNum } from '@/store/modules/user'
 import UserListSkeleton from '@/components/skeleton/user-list'
@@ -19,15 +18,18 @@ const UserList: FC<UserListProps> = ({ width }) => {
   const dispatch = useDispatch()
 
   const { id: storeId } = useSelector((state: AppState) => state.user.userInfo)
-  const [loading, setLoading] = useState(true)
-  const [userList, setUserList, updateItem] = useMap<UserItemInfo>([])
   const [current, setCurrent] = useState(1)
+  const [recommendUserList, setRecommendUserList] = useState<
+    {
+      page: number
+      list: UserItemInfo[]
+    }[]
+  >([{ page: current, list: [] }])
   const atBottom = useAtBottom()
   const [isFinal, setIsFinal] = useState(false)
 
   // 获取推荐用户列表
   const getRecommendUserList = async () => {
-    setLoading(true)
     try {
       const { data } = await getRecommendUserListAPI({ current, pageSize: 6 })
       if (data.length < 6) setIsFinal(true)
@@ -39,12 +41,19 @@ const UserList: FC<UserListProps> = ({ width }) => {
           return { ...user, works }
         }),
       )
-      setUserList([...Array.from(userList.values()), ...userSource])
+      setRecommendUserList((prev) => {
+        const result = prev.map((item) => {
+          if (item.page === current) {
+            return { page: item.page, list: userSource }
+          }
+          return item
+        })
+        result.push({ page: current + 1, list: [] })
+        return result
+      })
     } catch (error) {
       console.log('出现错误了喵！！', error)
       return
-    } finally {
-      setLoading(false)
     }
   }
 
@@ -56,34 +65,64 @@ const UserList: FC<UserListProps> = ({ width }) => {
     getRecommendUserList()
   }, [current])
 
-  const handleFollow = async (id: string) => {
+  const handleFollow = async (page: number, id: string) => {
     if (id === storeId) {
       message.error('不能关注自己')
       return
     }
+    // 获取到对应的userItem
+    const userItem = recommendUserList
+      .find((item) => item.page === page)!
+      .list.find((user) => user.id === id)
     try {
       await userActionsAPI({ id })
-      if (!userList.get(id)!.isFollowing) {
+      if (!userItem!.isFollowing) {
         dispatch(increaseFollowNum())
       } else {
         dispatch(decreaseFollowNum())
       }
-      updateItem(id, { ...userList.get(id)!, isFollowing: !userList.get(id)!.isFollowing })
+      setRecommendUserList(
+        recommendUserList.map((item) => {
+          if (item.page === page) {
+            return {
+              page: item.page,
+              list: item.list.map((user) =>
+                user.id === id ? { ...user, isFollowing: !user.isFollowing } : user,
+              ),
+            }
+          }
+          return item
+        }),
+      )
     } catch (error) {
       console.log('出现错误了喵！！', error)
       return
     }
   }
 
-  const handleLikeWork = async (userId: string, workId: string) => {
+  const handleLikeWork = async (page: number, userId: string, workId: string) => {
     try {
       await likeActionsAPI({ id: workId })
-      updateItem(userId, {
-        ...userList.get(userId)!,
-        works: userList
-          .get(userId)!
-          .works!.map((work) => (work.id === workId ? { ...work, isLiked: !work.isLiked } : work)),
-      })
+      setRecommendUserList(
+        recommendUserList.map((item) => {
+          if (item.page === page) {
+            return {
+              page: item.page,
+              list: item.list.map((user) =>
+                user.id === userId
+                  ? {
+                      ...user,
+                      works: user.works!.map((work) =>
+                        work.id === workId ? { ...work, isLiked: !work.isLiked } : work,
+                      ),
+                    }
+                  : user,
+              ),
+            }
+          }
+          return item
+        }),
+      )
     } catch (error) {
       console.log('出现错误了喵！！', error)
       return
@@ -92,39 +131,28 @@ const UserList: FC<UserListProps> = ({ width }) => {
 
   return (
     <div className='relative w-full p-5 min-h-125'>
-      <CSSTransition
-        in={userList.size !== 0 && !loading}
-        timeout={300}
-        classNames='opacity-gradient'
-        unmountOnExit>
-        <div className='relative w-full flex flex-col gap-20px'>
-          {Array.from(userList.values()).map((item) => (
-            <UserItem
-              key={item.id}
-              {...item}
-              width={width}
-              follow={handleFollow}
-              likeWork={handleLikeWork}
-            />
-          ))}
-        </div>
-      </CSSTransition>
+      {recommendUserList.map((everyPage) => (
+        <CSSTransition
+          key={everyPage.page}
+          in={everyPage.list.length !== 0}
+          timeout={300}
+          classNames='opacity-gradient'
+          unmountOnExit>
+          <div className='relative w-full flex flex-col gap-20px'>
+            {everyPage.list.map((user) => (
+              <UserItem
+                key={user.id}
+                {...user}
+                width={width}
+                follow={(id) => handleFollow(everyPage.page, id)}
+                likeWork={(userId, workId) => handleLikeWork(everyPage.page, userId, workId)}
+              />
+            ))}
+          </div>
+        </CSSTransition>
+      ))}
 
-      <CSSTransition
-        in={userList.size === 0 && !loading}
-        timeout={300}
-        classNames='opacity-gradient'
-        unmountOnExit>
-        <Empty />
-      </CSSTransition>
-
-      <CSSTransition
-        in={userList.size === 0 && loading}
-        timeout={300}
-        classNames='opacity-gradient'
-        unmountOnExit>
-        <UserListSkeleton className='absolute top-5' />
-      </CSSTransition>
+      <UserListSkeleton row={1} />
     </div>
   )
 }
--- src/components/explore/work-list/index.tsx
+++ src/components/explore/work-list/index.tsx
@@ -2,9 +2,8 @@ import { FC, useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import type { WorkNormalItemInfo } from '@/utils/types'
 import WorkNormalItem from '@/components/common/work-normal-item'
-import { useMap, useAtBottom } from '@/hooks'
+import { useAtBottom } from '@/hooks'
 import { getRecommendWorksAPI } from '@/apis'
-import Empty from '@/components/common/empty'
 import { likeActionsAPI } from '@/apis'
 import WorkListSkeleton from '@/components/skeleton/work-list'
 import { CSSTransition } from 'react-transition-group'
@@ -13,23 +12,33 @@ import { pushToRecommendWorkList, resetOtherList, setCurrentList } from '@/store
 const WorkList: FC = () => {
   const dispatch = useDispatch()
 
-  const [loading, setLoading] = useState(true)
-  const [workList, setWorkList, updateItem] = useMap<WorkNormalItemInfo>([])
   const [current, setCurrent] = useState(1)
+  const [recommendWorkList, setRecommendWorkList] = useState<
+    {
+      page: number
+      list: WorkNormalItemInfo[]
+    }[]
+  >([{ page: current, list: [] }])
   const atBottom = useAtBottom()
   const [isFinal, setIsFinal] = useState(false)
 
   const getRecommendWorks = async () => {
-    setLoading(true)
     try {
       const { data } = await getRecommendWorksAPI({ pageSize: 30, current })
       if (data.length < 30) setIsFinal(true)
-      setWorkList([...Array.from(workList.values()), ...data])
+      setRecommendWorkList((prev) => {
+        const result = prev.map((item) => {
+          if (item.page === current) {
+            return { page: item.page, list: data }
+          }
+          return item
+        })
+        result.push({ page: current + 1, list: [] })
+        return result
+      })
     } catch (error) {
       console.log('出现错误了喵！！', error)
       return
-    } finally {
-      setLoading(false)
     }
   }
 
@@ -41,14 +50,32 @@ const WorkList: FC = () => {
     getRecommendWorks()
   }, [current])
 
-  const handleLike = async (id: string) => {
+  const handleLike = async (page: number, id: string) => {
     await likeActionsAPI({ id })
-    updateItem(id, { ...workList.get(id)!, isLiked: !workList.get(id)!.isLiked })
+    setRecommendWorkList(
+      recommendWorkList.map((item) => {
+        if (item.page === page) {
+          return {
+            page: item.page,
+            list: item.list.map((work) => {
+              if (work.id === id) {
+                return { ...work, isLiked: !work.isLiked }
+              }
+              return work
+            }),
+          }
+        }
+        return item
+      }),
+    )
   }
 
   const addRecommendWorks = () => {
     dispatch(resetOtherList())
-    dispatch(pushToRecommendWorkList(Array.from(workList.values()).map((item) => item.id)))
+    const result = recommendWorkList.reduce((prev, current) => {
+      return prev.concat(current.list.map((item) => item.id))
+    }, [] as string[])
+    dispatch(pushToRecommendWorkList(result))
     dispatch(setCurrentList('recommendWorkList'))
   }
 
@@ -58,38 +85,27 @@ const WorkList: FC = () => {
         <span>推荐作品</span>
       </div>
 
-      <CSSTransition
-        in={workList.size !== 0 && !loading}
-        timeout={300}
-        classNames='opacity-gradient'
-        unmountOnExit>
-        <div className='relative w-full flex flex-wrap gap-5'>
-          {Array.from(workList.values()).map((work) => (
-            <WorkNormalItem
-              key={work.id}
-              itemInfo={work}
-              like={handleLike}
-              onClick={addRecommendWorks}
-            />
-          ))}
-        </div>
-      </CSSTransition>
+      {recommendWorkList.map((everyPage) => (
+        <CSSTransition
+          key={everyPage.page}
+          in={everyPage.list.length !== 0}
+          timeout={300}
+          classNames='opacity-gradient'
+          unmountOnExit>
+          <div className='relative w-full flex flex-wrap gap-5'>
+            {everyPage.list.map((work) => (
+              <WorkNormalItem
+                key={work.id}
+                itemInfo={work}
+                like={(id) => handleLike(everyPage.page, id)}
+                onClick={addRecommendWorks}
+              />
+            ))}
+          </div>
+        </CSSTransition>
+      ))}
 
-      <CSSTransition
-        in={workList.size === 0 && !loading}
-        timeout={300}
-        classNames='opacity-gradient'
-        unmountOnExit>
-        <Empty />
-      </CSSTransition>
-
-      <CSSTransition
-        in={workList.size === 0 && loading}
-        timeout={300}
-        classNames='opacity-gradient'
-        unmountOnExit>
-        <WorkListSkeleton className='absolute top-14' />
-      </CSSTransition>
+      <WorkListSkeleton row={1} />
     </div>
   )
 }
