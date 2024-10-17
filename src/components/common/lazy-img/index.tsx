98133799984c960d3d25bd630f07f035ef3df79d
non_hana <1209220829@qq.com>
2024-10-17 21:05:42 +0800

    feat: 完成图片懒加载

--- src/components/common/lazy-img/index.tsx
+++ src/components/common/lazy-img/index.tsx
@@ -1,5 +1,5 @@
 import ImgLoadingSkeleton from '@/components/skeleton/img-loading'
-import { FC, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { CSSTransition } from 'react-transition-group'
 
 type LazyImgProps = {
@@ -17,24 +17,51 @@ const LazyImg: FC<LazyImgProps> = ({
   alt,
 }) => {
   const [imgLoading, setImgLoading] = useState(true)
+  const [isInView, setIsInView] = useState(false)
+  const imgContainerRef = useRef<HTMLDivElement>(null)
+  const imgRef = useRef<HTMLImageElement>(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(
+        (entry) => {
+          if (entry.isIntersecting) {
+            setIsInView(true)
+            observer.disconnect()
+          }
+        },
+        { threshold: 0.1 },
+      )
+    })
+
+    if (imgContainerRef.current) observer.observe(imgContainerRef.current)
+
+    return () => {
+      if (imgContainerRef.current) observer.unobserve(imgContainerRef.current)
+    }
+  }, [])
 
   return (
     <div
+      ref={imgContainerRef}
       style={{
         width: typeof width === 'number' ? `${width}px` : width,
         height: typeof height === 'number' ? `${height}px` : height,
       }}
       className={`relative overflow-hidden ${className}`}>
-      <img
-        className='object-cover w-full h-full'
-        src={src}
-        alt={alt}
-        loading='lazy'
-        onLoad={() => {
-          setImgLoading(false)
-          imgLoaded && src && imgLoaded(src)
-        }}
-      />
+      {isInView && (
+        <img
+          ref={imgRef}
+          className='object-cover w-full h-full'
+          src={src}
+          alt={alt}
+          loading='lazy'
+          onLoad={() => {
+            setImgLoading(false)
+            imgLoaded && src && imgLoaded(src)
+          }}
+        />
+      )}
       <CSSTransition in={imgLoading} timeout={300} classNames='opacity-gradient' unmountOnExit>
         <ImgLoadingSkeleton className='absolute top-0 left-0' />
       </CSSTransition>
