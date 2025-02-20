3a125eb35e07d3741dd3cb8e91b636b46b9d0739
non_hana <1209220829@qq.com>
2025-02-20 16:00:05 +0800

    chore: 移除 stylelint 配置及相关依赖

--- .stylelintrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['stylelint-config-standard-less', 'stylelint-config-recess-order'],
-  overrides: [{ files: ['**/*.less'], customSyntax: 'postcss-less' }],
-  plugins: [],
-  rules: {},
-}
